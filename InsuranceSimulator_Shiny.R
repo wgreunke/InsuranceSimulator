d1b6044c8105e02fad1ac7e5548a85bc5fab5a3c
wgreunke <wgreunke@gmail.com>
2021-12-10 20:21:18 -0800

    Data frames working, starting to buy new polices

--- InsuranceSimulator_Shiny.R
+++ InsuranceSimulator_Shiny.R
@@ -47,17 +47,35 @@ fluidRow(column(12, tableOutput("table"))), #end fluid row
 # Define server logic required to draw a histogram
 server <- function(input, output) {
 #  https://stackoverflow.com/questions/68405253/r-shiny-variable-increases-by-1-each-time-actionbutton-is-clicked
-#create the reactive value object.
-rv=reactiveValues( company_df=data.frame("num_customers" = c(7,9), "new_customers"=1:2, "new_claims" = c(0,1),"week"=(1:2),
-                        policy_df=data.frame("week"=0,"num_customers"=0),
-                        claims_df=data.frame("week"=0,"num_claims"=0,"avg_claim"=0))
-                        )#end reactive values
-#Do the work with the reactive value when the button is pushed
 
+#Variables
+target_policy_price=600 #This is what the customer is willing to pay, it has to be discovered
+displayed_price = 601 #What price is advertised to customer.  Will be controlled by user.
+num_shopping_customers=10 #How many customers are shopping for a policy?
+
+
+#create the reactive value object
+rv=reactiveValues( company_df=data.frame("num_customers" = c(7,9), "new_customers"=1:2, "new_claims" = c(0,1),"week"=(1:2)),
+                        policy_df=data.frame("week"=0,"num_customers"=0),
+                        claims_df=data.frame("week"=0,"num_claims"=0,"avg_claim"=0),
+                        current_week=0
+                        )#end reactive values
+
+#Do the work with the reactive value when the button is pushed
 #Advance the cycle one week
 observeEvent(input$add_week, {
-  rv$company_df=rv$company_df+1
+  rv$current_week=rv$current_week+1
   rv$policy_df=rv$policy_df+1
+  #shop for policy
+  if (displayed_price<target_policy_price+20)
+  {
+    #Buy the policy, add a row
+    new_customer=c(rv$current_week,1)
+    cbind(rv$current_week,new_customer)
+  }#End if
+  
+  
+  rv$company_df=rv$company_df+1
   rv$claims_df=rv$claims_df+1
 }) # End observeEvent
 
@@ -66,17 +84,10 @@ observeEvent(input$add_week, {
 #observeEvent(input$add_week,{react_values$week=react_values$week+1})
   
 
-
-policy_data=data.frame("week"=0,"customer_id" =0, "annual_premium"=0 )
-#output$table <- renderTable(iris)  
-#output$table=renderTable(policy_data)
-    print(company_data)
-  
-
+#output$table=renderTable(rv$company_df)
 output$table=renderTable(rv$policy_df)
-
     
-#Policy plot is total number policy holders and policy price history.
+#Policy plot is total number policy holders and policy price history
   output$policy_plot <- renderPlot({
     # generate bins based on input$bins from ui.R
     x    <- faithful[, 2]
@@ -99,8 +110,10 @@ output$table=renderTable(rv$policy_df)
     #plot(rv$company_df$week,rv$company_df$num_customers)
     
     #ggplot(company_data, aes(x=week,y=total_customers))+geom_line()
-    ggplot(rv$company_df, aes(x=week,y=num_customers))+geom_line()
-  })
+    #ggplot(rv$company_df, aes(x=week,y=num_customers))+geom_line()
+    ggplot(rv$claims_df,aes(x=week,y=num_claims)) +geom_line()
+    #ggplot(rv$claims_df, aes(x=week,y=num_claims))+geom_line()
+      })
   
   #This shows the financial data
   #Main rows - DWP, Claims Payout, and Surplus?
@@ -112,8 +125,10 @@ output$table=renderTable(rv$policy_df)
     
     # draw the histogram with the specified number of bins
     #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    ggplot(company_data, aes(x=week,y=total_customers))+geom_line()
-  })
+    #ggplot(company_data, aes(x=week,y=total_customers))+geom_line()
+    ggplot(rv$company_df, aes(x=week,y=num_customers))+geom_line()
+    
+    })
 
   #Major metric is Loss Ratio
   
@@ -124,7 +139,7 @@ output$table=renderTable(rv$policy_df)
     
     # draw the histogram with the specified number of bins
     #hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    ggplot(company_data, aes(x=week,y=total_customers))+geom_line()
+    #ggplot(company_data, aes(x=week,y=total_customers))+geom_line()
   })
   
 } #End Server
